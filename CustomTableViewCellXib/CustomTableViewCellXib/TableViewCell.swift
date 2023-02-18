190accc74351bb288a115d91d40aa556e2075cde
Shulun Lyu <lyu.shul@northeastern.edu>
2023-02-17 23:48:53 -0800

    Checking in code for Custom table view cell xib

---
+++ CustomTableViewCellXib/CustomTableViewCellXib/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  CustomTableViewCellXib
+//
+//  Created by Shulun Lyu on 2/17/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageContainer: UIImageView!
+    
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+  
+    
+}
